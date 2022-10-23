7d3835608d7d9e97b904672e33c9e0d4df8a292f
cjhet02 <cjhet02@gmail.com>
2022-10-23 12:38:38 -0700

    implement python script with console io

--- curator.py
+++ curator.py
@@ -1,19 +1,37 @@
 import requests
-url = "https://scoresaber.com"
-# username = "Lorft" #just for testing, will be recieved through form
-# Params = {'search':username}
-# user = requests.get(url + "/api/players", params=Params)
-# res = user.json()
-# id = res['players'][0]['id']
-id = sys.argv[1]
+#get user's preferences with no wait
+user = input("Enter Scoresaber profile url: ")
+lim = input("Enter number of scores to consider: ")
+#starRange = input("")
+starBound = 0.5
 
-lim = 10
-Params = {'limit': lim+1}
-scores = requests.get(url + "/api/player/" + id + "/scores", params=Params)
+#parse user id from url
+id = ""
+for i in reversed(range(len(user))) :
+    if user[i] == '/':
+        break
+    id = user[i] + id
+
+
+Params = {'limit': lim}
+scores = requests.get("https://scoresaber.com/api/player/" + id + "/scores", params=Params)
 list = scores.json()
 starSum = 0.00
 for i in list['playerScores']:
-    print("%s --- %d - %d pp" %(i['leaderboard']['songName'], i['score']['baseScore'], i['score']['pp']))
+    print("%s - %.2f --- #%d - %d pp" %(i['leaderboard']['songName'], i['leaderboard']['stars'], i['score']['rank'], i['score']['pp']))
     starSum += i['leaderboard']['stars']
 
-print("Average Star Rating: %d" %(starSum/lim))
+avStar = starSum/float(lim)
+print("\nAverage Star Rating: %.2f" %(avStar))
+
+Params = {
+    'ranked': True,
+    'minStar': avStar - starBound,
+    'maxStar': avStar + starBound,
+    'category': 2 #sorting by scores set (aka popularity)
+}
+songs = requests.get("https://scoresaber.com/api/leaderboards/", params=Params)
+list = songs.json()
+print("Your Recommendations:\n")
+for i in list['leaderboards']:
+    print("%s - %s --- %.2f " %(i['songName'], i['levelAuthorName'], i['stars']))
